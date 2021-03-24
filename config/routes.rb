68232dbdbccdf42a11dc6e72c4fd24b041f8c0a9
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-24 16:51:18 -0500

    Add api namespace to router

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
+  end
 end
