fbd73932db440f0f5af3d4d4c968a1d8a8eb7fce
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-24 23:12:39 -0500

    Add create function to be able to sabe an new sale in the user account

--- app/controllers/api/sales_controller.rb
+++ app/controllers/api/sales_controller.rb
@@ -1,6 +1,17 @@
 class Api::SalesController < ApiController
   before_action :set_sale, only: %i[show]
 
+  def create
+    user = User.find(params[:user_id])
+    sale = user.sales.create(sale_params)
+    if sale.valid?
+      sale.save
+      render json: sale, status: :created
+    else
+      render json: sale.errors, status: :unprocessable_entity
+    end
+  end
+
   def index
     render json: Sale.all
   end
@@ -10,6 +21,10 @@ class Api::SalesController < ApiController
   end
 
   private
+    def sale_params
+      params.require(:sale).permit(:total, :code)
+    end
+
     def set_sale
       @sale = Sale.find(params[:id])
     end
