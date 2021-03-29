6481116593ee66bd70390444e5f54ca0a33dec0d
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-29 11:01:02 -0500

    Add birth_date to params permited

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,6 @@
 
 class Api::UsersController < ApiController
   skip_before_action :authorize, only: %i[create]
-  before_action :set_user, only: %i[show update]
 
   def create
     user = User.create(user_params)
@@ -15,28 +14,39 @@ class Api::UsersController < ApiController
   end
 
   def show
-    render json: @user, only: %i[id name email direction role created_at]
+    render json: current_user, only: %i[id name email direction role created_at], methods: [:get_url]
   end
 
   def index
     users = User.all
-    render json: users, only: %i[id name email direction role created_at]
+    render json: users, except: %i[token password_digest]
   end
 
   def update
-    if @user.update(user_params)
-      render json: @user, only: %i[id name email direction role token created_at updated_at]
-    else
-      render json: @user.errors, status: :unprocessable_entity
+    puts '//////////////////////////////////////////////////////////////////////'
+    if params[:avatar]
+      current_user.avatar.purge if current_user.avatar.attached?
+      current_user.avatar.attach params[:avatar]
     end
+
+    user_params = {}
+    user_params[:name] = params[:name]  if params[:name]
+    user_params[:email] = params[:email]  if params[:email]
+    user_params[:direction] = params[:direction]  if params[:direction]
+    user_params[:role] = params[:role]  if params[:role]
+    current_user.update user_params
+
+    puts '//////////////////////////////////////////////////////////////////////'
+    # if @user.update(user_params)
+    render json: current_user, except: %i[password_digest], methods: :get_url
+    # else
+    #   render json: @user.errors, status: :unprocessable_entity
+    # end
+    puts '//////////////////////////////////////////////////////////////////////'
   end
 
   private
     def user_params
-      params.require(:user).permit(:name, :email, :direction, :role, :profile, :password)
-    end
-
-    def set_user
-      @user = User.find(params[:id])
+      params.require(:user).permit(:name, :email, :direction, :role, :profile, :password, :birth_date)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   has_secure_password
   has_secure_token
 
-  has_one_attached :profile
+  has_one_attached :avatar
   has_many :sales
   has_many :reviews
   has_many :carts
   has_many :favorites
   has_many :drinks_cart, through: :carts, source: :drink
   has_many :drinks_favorite, through: :favorites, source: :drink
-  
 
   validates :name, presence: true, length: { minimum: 2, maximum: 60 }
   validates :email, presence: true, email: true, uniqueness: true
   validates :direction, length: { minimum: 3 }, allow_blank: true
 
   validates :birth_date, presence: true
-  validate :under_age 
+  validate :under_age
 
   enum role: { member: 0, admin: 1 }
 
@@ -26,12 +27,15 @@ class User < ApplicationRecord
     update(token: nil)
   end
 
-  
-  private
 
-  def under_age
-    return if !birth_date.nil? && birth_date <= 18.years.ago
-
-    errors.add(:birth_date, 'You should be 18 years old to create and account')
+  def get_url
+    'url_for(self.avatar)'
   end
+
+  private
+    def under_age
+      return if !birth_date.nil? && birth_date <= 18.years.ago
+
+      errors.add(:birth_date, 'You should be 18 years old to create and account')
+    end
 end
