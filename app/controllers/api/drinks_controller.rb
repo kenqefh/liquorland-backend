8641b8b1a021dfc488335826474b7e2732378f6b
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-04-01 12:36:49 -0500

    Add best sellings endpoint that is located in drinks_controller

--- app/controllers/api/drinks_controller.rb
+++ app/controllers/api/drinks_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::DrinksController < ApiController
   before_action :set_drink, only:  %i[show]
-  skip_before_action :authorize, only: %i[index show]
+  skip_before_action :authorize, only: %i[index show best_sellings]
 
   def index
     drinks = Drink.all
@@ -26,6 +26,23 @@ class Api::DrinksController < ApiController
       reviews: { except: %i[user_id drink_id], include: { user: { only: %i[id name], methods: :avatar_url } } }
     },
     methods: %i[rating_avg image_url]
+  end
+
+  def best_sellings
+    drinks = Drink
+    .left_joins(:sale_drinks)
+    .group(:id)
+    .order('COUNT(sale_drinks.id) DESC')
+    .limit(params[:limit] || 3)
+
+    result = {
+      id: 1,
+      name: 'Best sellings',
+      description: 'Reality is an illusion that occurs due to lack of alcohol.',
+      drinks: drinks,
+      drinks2: drinks
+    }
+    render json: result, methods: [:image_url, :rating_avg]
   end
 
   private
