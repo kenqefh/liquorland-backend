0794eb0c4487e75429518e75cc37459e34b5e00b
Frank Dominguez <elvisf.dominguez@gmail.com>
2021-03-31 21:22:23 -0500

    Seeder was updated, add cover_url method to retrieve the category image and controller was updated

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,12 @@ class Category < ApplicationRecord
 
   has_one_attached :cover, dependent: :destroy
   has_many :drinks, dependent: :destroy
+
+  def cover_url
+    if cover.attached?
+      Rails.application.routes.default_url_options[:host] = 'https://liquorland-backend.herokuapp.com'
+      # Rails.application.routes.default_url_options[:host] = 'http://localhost:3000'
+      Rails.application.routes.url_helpers.url_for(cover)
+    end
+  end
 end
